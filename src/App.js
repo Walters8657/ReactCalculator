17b5bd06ce527e51efd7ba82c1f7dc81e318440e
Michael Walters <waltersm8@winthrop.edu>
2021-06-01 11:50:31 -0400

    Working calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Display from "./Display"
+import ButtonPanel from "./ButtonPanel";
+import calculate from "./logic/calculate";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.js";
+
+export default class App extends React.Component {
+  state = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  };
+
+  render() {
+    return (
+      <div className = "component-App">
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ src/logic/isNumber.js
@@ -0,0 +1,8 @@
+//
+// Logic code directly copied from https://github.com/ahfarmer/calculator/tree/master/src/logic
+//
+//----------------------------------------------------------------------------------------------
+
+export default function isNumber(item) {
+    return /[0-9]+/.test(item);
+  }
